407e614b1055f9ba8a7c88ca91e89369db467c02
vbronfman <vbronfman@users.noreply.github.com>
2020-12-05 21:39:22 +0200

    Update build_test.sh

--- build_test.sh
+++ build_test.sh
@@ -1,4 +1,9 @@
 #!bash
+# bogus test prints the hash of the current git commit and waits ~3 min It is expected to always pass 
 
 echo Test on commit
+echo $(git rev-parse HEAD)
 sleep ${timeout=3}
+
+
+
